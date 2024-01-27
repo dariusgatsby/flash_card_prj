7d629ccdfccb391603c4086d71b4d62bcdc930ed
dariusgatsby <145069350+dariusgatsby@users.noreply.github.com>
2024-01-26 22:50:48 -0800

    created window, added front of card

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
 
+# Window config
+BACKGROUND_COLOR = "#B1DDC6"
+window = Tk()
+window.title("Flash Card App")
+window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
+
+
+# Card photo
+card_front = PhotoImage(file="images/card_front.png")
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_image(400, 263, image=card_front)
+canvas.pack()
+
+window.mainloop()
