cee13b44c5e52e3217ae7f0bd9b61f0a57bc002b
dgatsby <dariusid08@gmail.com>
2024-01-28 00:21:50 -0800

    added known or not known variable

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from tkinter import *
 import pandas
 import random
 
-words = pandas.read_csv("data/spanish_words.csv").to_dict(orient="records")
+words_to_learn = pandas.read_csv("data/spanish_words.csv").to_dict(orient="records")
 current_card = {}
+known_words = []
+
+
+
+def knew_word():
+    global current_card, known_words
+    known = current_card
+    words_to_learn.remove(known)
+    known_words.append(known)
+    print(known)
+    known_words = pandas.DataFrame(known).to_csv("known_words.csv", index=False)
+    flip_card()
 
 
 def flip_card():
@@ -14,13 +26,16 @@ def flip_card():
 
 
 # Word generation
-def generate_word():
-    global current_card, flip_timer
-    canvas.itemconfig(card, image=card_front)
-    current_card = random.choice(words)
-    canvas.itemconfig(lang_word, text=current_card["Spanish"], fill="black")
-    canvas.itemconfig(lang_name, text="Spanish", fill="black")
-    flip_timer = window.after(3000, func=flip_card)
+def generate_word(y_n):
+    if y_n == 'y':
+        knew_word()
+    else:
+        global current_card, flip_timer
+        canvas.itemconfig(card, image=card_front)
+        current_card = random.choice(words_to_learn)
+        canvas.itemconfig(lang_word, text=current_card["Spanish"], fill="black")
+        canvas.itemconfig(lang_name, text="Spanish", fill="black")
+        flip_timer = window.after(3000, func=flip_card)
 
 
 # Window config
@@ -41,11 +56,11 @@ canvas.grid(column=0, row=0, columnspan=2)
 # Buttons
 right_img = PhotoImage(file="images/right.png")
 wrong_img = PhotoImage(file="images/wrong.png")
-right_button = Button(image=right_img, borderwidth=0, highlightthickness=0, command=generate_word)
-wrong_button = Button(window, image=wrong_img, borderwidth=0, highlightthickness=0, command=generate_word)
+right_button = Button(image=right_img, borderwidth=0, highlightthickness=0, command=lambda i="y": generate_word(i))
+wrong_button = Button(window, image=wrong_img, borderwidth=0, highlightthickness=0, command=lambda i="n": generate_word(i))
 right_button.grid(column=1, row=1)
 wrong_button.grid(column=0, row=1)
 
-generate_word()
+generate_word(y_n="n")
 
 window.mainloop()
